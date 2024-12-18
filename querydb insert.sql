a5506883d435e4e6593f8f2df658359ec8f23a41
KeiKurono96 <116596679+KeiKurono96@users.noreply.github.com>
2024-12-18 09:32:10 -0500

    Add files via upload
    otros archivos del proyecto

---
+++ querydb insert.sql
@@ -0,0 +1,18 @@
+USE [DbAjsNet8]
+GO
+
+INSERT INTO [dbo].[Users]
+           ([Email]
+           ,[Password]
+           ,[Nombres]
+           ,[Apellidos]
+           ,[Sexo]
+           ,[Tipo])
+     VALUES
+           ('1'
+           ,'1'
+           ,'Main'
+           ,'Tester'
+           ,'m'
+           ,'adm')
+GO
